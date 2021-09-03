25e0a3084e16f2286e1b0e2a543cde76800df5c7
Austin May <austin.may@protonmail.com>
2021-09-02 23:46:25 -0400

    Added searchByDOB to searchByTrait. Added placeholder functions for search by parents & current spouse(Incomplete, working on matching spouse ID to firstName, lastName. same with parents.) Added displayFamily, displayDescendants place holder functions(incomplete, still sorting algorithim.)

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ function traitToSearchBy(people) {
         foundPeople = searchByGender(foundPeople);
         break;
       case 'DOB':
-
+      foundPeople = searchByDOB(foundPeople);
         break;
       case 'Height':
         foundPeople = searchByHeight(foundPeople);
@@ -59,8 +59,10 @@ function traitToSearchBy(people) {
         foundPeople = searchByOccupation(foundPeople);
         break;
       case 'Parents':
+        foundPeople = searchByParents(foundPeople);
         break;
       case 'Current Spouse':
+        foundPeople = searchByCurrentSpouse(foundPeople);
         break;
 
       default:
@@ -92,10 +94,10 @@ function mainMenu(person, people) {
       displayPerson(person);
       break;
     case "family":
-      // TODO: get person's family
+      displayFamily(person);
       break;
     case "descendants":
-      // TODO: get person's descendants
+      displayDescendants(person);
       break;
     case "restart":
       app(people); // restart
@@ -200,6 +202,41 @@ function searchByOccupation(people) {
   });
   return foundPerson;
 }
+
+function searchByDOB (people) {
+  let dob = promptFor("What is the persons date of birth?", autoValid);
+  let foundPerson = people.filter(function(potentialMatch){
+    if(potentialMatch.dob === dob) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  });
+  return foundPerson;
+  
+}
+
+//placeholder
+function searchByParents (people) {
+  let parents;
+  let foundPerson;
+return foundPerson;
+}
+
+//placeholder 
+function searchByCurrentSpouse (people) {
+    let currentSpouse = promptFor("Who is the persons spouse?", autoValid);
+    let foundPerson = people.filter(function(potentialMatch){
+      if(potentialMatch.currentSpouse === currentSpouse) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    
+    return foundPerson;
+}
 //TODO: add other trait filter functions here.
 
 
@@ -235,6 +272,19 @@ function displayPerson(person) {
   alert(personInfo);
 }
 
+
+//siblings = .parents / unfinshed function / placeholder
+function displayFamily(person){
+  let familyInfo = "Parents: " + person[0].parents + "\n";
+  familyInfo += "Current Spouse: " + person[0].currentSpouse + "\n";
+  alert(familyInfo);
+}
+
+//descendants - unfinished function / placeholder
+function displayDescendants(person){
+  let descendants;
+  alert(descendants);
+}
 //#endregion
 
 
